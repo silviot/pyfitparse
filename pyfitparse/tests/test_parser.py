4421609c03c8321b3e44462be28351d7b9e7fffd
Silvio Tomatis <silviot@gmail.com>
2013-06-17 23:20:39 +0200

    try to make py3k happy

--- pyfitparse/tests/test_parser.py
+++ pyfitparse/tests/test_parser.py
@@ -4,11 +4,11 @@ import os
 
 HERE = os.path.dirname(__file__)
 TESTFILE = os.path.join(HERE, 'data', 'Activity.fit')
-TESTDATA = open(TESTFILE).read()
+TESTDATA = open(TESTFILE, 'rb').read()
 
 class TestParser(unittest.TestCase):
     def test_basic(self):
         from pyfitparse.parser import parse_fit_data
         result = parse_fit_data(TESTDATA)
-        #Dummy test
+        #Dummy test_basic
         assert len(result.records) == 0
