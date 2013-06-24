0663830d9b18309784ff390531f8291764744386
Silvio Tomatis <silviot@gmail.com>
2013-06-24 14:27:13 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -3,17 +3,6 @@ from setuptools import setup
 from distutils.core import Command
 
 
-class PyTest(Command):
-    user_options = []
-    def initialize_options(self):
-        pass
-    def finalize_options(self):
-        pass
-    def run(self):
-        import sys,subprocess
-        errno = subprocess.call([sys.executable, 'runtests.py', 'activityextractor'])
-        raise SystemExit(errno)
-
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
@@ -22,7 +11,6 @@ setup(
     version='0.1',
     description='pyfitparse',
     long_description=README,
-    cmdclass={'test': PyTest},
-    packages=['fitparse', 'activityextractor'],
+    packages=['fitparse'],
     install_requires=['pytz'],
 )
